4ba01ead6955cbd2a815ad35d9d37b8ff3b88d66
wendel4891 <wendel4891@gmail.com>
2024-01-03 22:47:29 -0300

    Criado e configurado site de administração para o modelo Post, no arquivo blog/admin.py; foi criado também um usuário administrador, com o comando 'python manager.py createsuperuser'. Com esse usuário, é possíel acessar o site de administração, no domínio /admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from blog.models import Post
+
 # Register your models here.
+# Jeito mais simples de incluir o modelo no site de administração:
+''' admin.site.register(Post) '''
+
+# Jeito mais elaborado, de incluir o modelo, com personalizações:
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','publish','status')
+    list_filter = ('status','created','publish','author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status','publish')
