f07a5bb97a5dd0257c8387f658e65f25723cbe7d
wendel4891 <wendel4891@gmail.com>
2024-01-10 07:45:11 -0300

    Adicionado template de paginação: templates.patination.html. O template foi usado em post.list.html. E seu uso foi configurado na view que retorna o template list.html: post_list().

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
@@ -6,10 +7,21 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # três postagens em cada página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver fora do intervalo,
+        # exibe a última página de resultados
+        posts = paginator.page(paginator.num_pages)        
     return render(request,
                   'blog/post/list.html',
-                  {'posts':posts})
+                  {'page': page, 'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
