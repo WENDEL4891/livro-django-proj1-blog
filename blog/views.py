889824f21c6e21913d21762ff923095d641d3942
wendel4891 <wendel4891@gmail.com>
2024-01-11 20:14:10 -0300

    Criada view baseada em classe; criada rota, que usa a nova view; alterado o template list.html, para adequar o nome do objeto de paginação, que é padronizado na classe de view criada, devido a seus métodos e atributos herdados da classe ListView de django.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from django.views.generic import ListView
 
 from .models import Post
 
@@ -32,3 +33,9 @@ def post_detail(request, year, month, day, post):
     return render(request,
             'blog/post/detail.html',
             {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
